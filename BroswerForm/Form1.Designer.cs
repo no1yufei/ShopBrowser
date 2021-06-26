740931a43eebd87f164bdd6a52822f210a90edbd
yufei <yufei@bjepower.com>
2021-06-26 16:47:00 +0800

    1.添加加速悬浮球控件代码和浏览器Tab页控件框架

---
+++ BroswerForm/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace BroswerX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browserContainer1 = new BroswerX.UC.BrowserContainer();
+            this.SuspendLayout();
+            // 
+            // browserContainer1
+            // 
+            this.browserContainer1.BackColor = System.Drawing.Color.White;
+            this.browserContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browserContainer1.Location = new System.Drawing.Point(0, 0);
+            this.browserContainer1.Name = "browserContainer1";
+            this.browserContainer1.Size = new System.Drawing.Size(780, 414);
+            this.browserContainer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 414);
+            this.Controls.Add(this.browserContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UC.BrowserContainer browserContainer1;
+
+    }
+}
+
